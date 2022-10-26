53514c0eae9754075b94124220be5e47f75adf11
Vivian Zitek <vivian.zitek@gmail.com>
2022-10-26 12:14:15 -0600

    Add framework for temperature conversion methods.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,37 @@
 ﻿// CS 1400 - Lab 7: Conversions
 
+// Temperature Unit Conversion
+tuple FahrenheitToCelsius() {
 
+
+}
+
+tuple FahrenheitToKelvin() {
+
+
+}
+
+tuple CelsiusToFahrenheit() {
+
+
+}
+
+tuple CelsiusToKelvin() {
+
+
+}
+
+tuple KelvinToFahrenheit() {
+
+
+}
+
+tuple KelvinToCelsius() {
+
+
+}
+
+// ------------------------------------------------------------------
 
 enum UnitTemperature {
 
